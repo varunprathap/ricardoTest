6e98db788242d84c8de42f157db4a31d28c2c758
Clay <varunprathap10@gmail.com>
2018-07-08 18:50:26 +0400

    * DealUnitTest.csproj: Unit test for deal view model.
    * Test.cs:
    * packages.config:

--- DealUnitTest/Test.cs
+++ DealUnitTest/Test.cs
@@ -1,13 +1,53 @@
 ﻿using NUnit.Framework;
-using System;
+using FluentAssertions;
+using GalaSoft.MvvmLight.Views;
+using Moq;
+using Deals.ViewModel;
+using Deals;
+
 namespace DealUnitTest
 {
     [TestFixture()]
     public class Test
     {
-        [Test()]
-        public void TestCase()
+
+
+        private Mock<INavigationService> _mockNavigation;
+
+        [SetUp]
+
+        public void SetUp()
         {
+            //Mock navigation service from MVVMLight.
+            _mockNavigation = new Mock<INavigationService>();
         }
+
+        //Get a deal 
+        private Deal CreateDealModel()
+        {
+            var dvm = new DealViewModel(_mockNavigation.Object);
+            return dvm.Deals[0];
+
+        }
+
+        //Test method should return count of 6 deals.
+        [Test]
+        public void DealViewModelShouldReturnDealsCount()
+        {
+            var dvm = new DealViewModel(_mockNavigation.Object);
+            dvm.Deals.Count.Should().Be(6);
+        }
+
+        //Test method for check favourite.
+        [Test]
+        public void ExecuteTheFavouriteCommandShouldChangeDealProperty()
+        {
+            var detailVm = new DetailDealVM(_mockNavigation.Object);
+            detailVm.Favourite = false;
+            detailVm.AddFavourite.Execute(null);
+            detailVm.Favourite.Should().Be(true);
+
+        }
+
     }
 }
